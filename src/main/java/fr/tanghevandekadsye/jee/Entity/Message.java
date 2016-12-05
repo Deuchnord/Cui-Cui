e2de99f6f3d4ffb10aacfbd0833a52eedf34e32f
qvandekadsye <qvandekadsye@gmail.com>
2016-12-05 11:30:11 +0100

    Cretion de la classe message et de sa methode de repository

--- src/main/java/fr/tanghevandekadsye/jee/Entity/Message.java
+++ src/main/java/fr/tanghevandekadsye/jee/Entity/Message.java
@@ -1,7 +1,98 @@
 package fr.tanghevandekadsye.jee.Entity;
 
+import org.springframework.data.annotation.Id;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import java.util.Date;
+import java.util.List;
+
 /**
  * Created by quentinvdk on 05/12/16.
+ * Représente un message sur le réseau social
  */
 public class Message {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    protected long id;
+    protected User author;
+    protected Date date;
+    protected List<String> hashtags;
+    protected List<User> likedBy;
+    protected String text;
+
+    protected Message () {}
+
+    public Message (User auth, Date dateOfMessage, List<String> hashtags,List<User> likes, String text)
+    {
+        this.author = auth;
+        this.date = dateOfMessage;
+        this.hashtags = hashtags;
+        this.likedBy = likes;
+        this.text = text;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public User getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(User author) {
+        this.author = author;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public List<String> getHashtags() {
+        return hashtags;
+    }
+
+    public void setHashtags(List<String> hashtags) {
+        this.hashtags = hashtags;
+    }
+
+    public List<User> getLikedBy() {
+        return likedBy;
+    }
+
+    public void setLikedBy(List<User> likedBy) {
+        this.likedBy = likedBy;
+    }
+
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+
+
+    @Override
+    public String toString() {
+        return "Message{" +
+                "id=" + id +
+                ", author=" + author +
+                ", date=" + date +
+                ", hashtags=" + hashtags +
+                ", likedBy=" + likedBy +
+                ", text='" + text + '\'' +
+                '}';
+    }
+
 }
--- src/main/java/fr/tanghevandekadsye/jee/Interfaces/Repository/MessageRepository.java
+++ src/main/java/fr/tanghevandekadsye/jee/Interfaces/Repository/MessageRepository.java
@@ -4,8 +4,14 @@ import fr.tanghevandekadsye.jee.Entity.Message;
 
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 /**
  * Created by quentinvdk on 05/12/16.
  */
 public interface MessageRepository extends CrudRepository<Message,Long> {
+
+    List<Message> findByHashtag(String hashtag);
+    List<Message> findByText (String Text);
+
 }
