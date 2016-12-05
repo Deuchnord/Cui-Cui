ac53eea6842cf5ab67313996ad4fa63763ba2aaa
Jérôme Deuchnord <conact@deuchnord.tk>
2016-12-05 10:46:04 +0100

    Implémentation de la classe User

--- src/main/java/fr/tanghevandekadsye/jee/Entity/User.java
+++ src/main/java/fr/tanghevandekadsye/jee/Entity/User.java
@@ -1,7 +1,25 @@
 package fr.tanghevandekadsye.jee.Entity;
 
+import org.springframework.data.annotation.Id;
+
 /**
  * Created by quentinvdk on 05/12/16.
  */
 public class User {
+    @Id
+    protected Long id;
+    protected String nom, pseudo, photoUrl;
+
+    public User() {}
+
+    public User(String nom, String pseudo, String photoUrl) {
+        this.nom = nom;
+        this.pseudo = pseudo;
+        this.photoUrl = photoUrl;
+    }
+
+    public Long getId() { return id; }
+    public String getNom() { return nom; }
+    public String getPseudo() { return pseudo; }
+    public String getPhotoUrl() { return photoUrl; }
 }
