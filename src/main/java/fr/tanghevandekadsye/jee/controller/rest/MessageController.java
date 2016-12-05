61a2f49a79394da9a952b386c45d33ab3195cd59
qvandekadsye <qvandekadsye@gmail.com>
2016-12-05 17:30:00 +0100

    Creation de l'api Rest des Messages

---
+++ src/main/java/fr/tanghevandekadsye/jee/controller/rest/MessageController.java
@@ -0,0 +1,69 @@
+package fr.tanghevandekadsye.jee.controller.rest;
+
+import fr.tanghevandekadsye.jee.Interfaces.Repository.MessageRepository;
+import fr.tanghevandekadsye.jee.entity.Message;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by quentinvdk on 05/12/16.
+ * Api rest des messages
+ */
+
+@RestController
+@RequestMapping("/api/messages")
+public class MessageController {
+    @Autowired
+    private MessageRepository repository;
+
+    /**
+     * Obtenir un message par son id
+     * @param id l'id du message
+     * @return Le message
+     */
+    @RequestMapping("{id}")
+    public Message getMessage(@PathVariable(value = "id") long id)
+    {
+        return repository.findOne(id);
+    }
+
+    @RequestMapping("/")
+    public Iterable<Message> getAll()
+    {
+        return repository.findAll();
+    }
+
+    @RequestMapping("hashtag/{name}")
+    public Iterable<Message> getByHashtags(@PathVariable(value = "name") String hashtag)
+    {
+        return repository.findByHashtag(hashtag);
+    }
+
+    @RequestMapping("search/{text}")
+    public Iterable<Message> getByText(@PathVariable(value = "text") String text)
+    {
+        return repository.findByText(text);
+    }
+    /*
+    Méthodes POST
+     */
+
+    @RequestMapping(value = "{message}",method = RequestMethod.POST)
+    public void addMessage(@PathVariable(value = "message") String message)
+    {
+        Message msg = new Message(author, message);
+        repository.save(msg);
+
+    }
+    @RequestMapping(value = "{id}", method = RequestMethod.DELETE)
+    public void deleteMessage(@PathVariable(value = "id") long id)
+    {
+        repository.delete(id);
+}
+
+
+
+}
