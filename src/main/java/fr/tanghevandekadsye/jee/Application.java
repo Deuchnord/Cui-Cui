7cacb74359c73982f03c208f67b00cae3f9cde84
qvandekadsye <qvandekadsye@gmail.com>
2016-12-06 10:30:51 +0100

    gestion des packages par l'application

--- src/main/java/fr/tanghevandekadsye/jee/Application.java
+++ src/main/java/fr/tanghevandekadsye/jee/Application.java
@@ -3,9 +3,11 @@ package fr.tanghevandekadsye.jee;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScans;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@ComponentScan("fr.tanghevandekadsye.jee.Interfaces.Repository")
+@ComponentScan({"fr.tanghevandekadsye.jee.Interfaces.Repository","fr.tanghevandekadsye.jee.controller.html"})
 public class Application {
 
     public static void main(String[] args) {
