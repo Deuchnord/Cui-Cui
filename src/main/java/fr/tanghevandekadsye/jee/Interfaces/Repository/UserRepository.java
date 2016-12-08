0c90a949f173dfe3b927232a5ceeb93f59d33c4a
qvandekadsye <qvandekadsye@gmail.com>
2016-12-08 10:24:44 +0100

    finition de l'api pour les Users

--- src/main/java/fr/tanghevandekadsye/jee/Interfaces/Repository/UserRepository.java
+++ src/main/java/fr/tanghevandekadsye/jee/Interfaces/Repository/UserRepository.java
@@ -3,6 +3,8 @@ package fr.tanghevandekadsye.jee.Interfaces.Repository;
 import fr.tanghevandekadsye.jee.entity.User;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.ArrayList;
+
 
 /**
  * Created by quentinvdk on 05/12/16.
@@ -10,4 +12,8 @@ import org.springframework.data.repository.CrudRepository;
 public interface UserRepository  extends CrudRepository<User,Long> {
 
     User findByToken(String token);
+    User findByPseudoAndPassword(String pseudo, String password);
+    User findByPseudoAndPasswordAndToken(String pseudo, String password,String token);
+    ArrayList<User> findByPseudo(String pseudo);
+    User findByPseudoAndToken(String pseudo, String token);
 }
--- src/main/java/fr/tanghevandekadsye/jee/controller/rest/UserController.java
+++ src/main/java/fr/tanghevandekadsye/jee/controller/rest/UserController.java
@@ -1,8 +1,12 @@
 package fr.tanghevandekadsye.jee.controller.rest;
 
 import fr.tanghevandekadsye.jee.Interfaces.Repository.UserRepository;
+
+import fr.tanghevandekadsye.jee.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -16,6 +20,46 @@ public class UserController {
     @Autowired
     private UserRepository userRepository;
 
-    @RequestMapping("/{token}")
+    /**
+     * Permet à l'utilisateur d'obtenir un token pour l'api
+     * @param username
+     * @param password
+     */
+    @RequestMapping(value = "/obtainToken/{username}/{password}",method = RequestMethod.POST)
+    public void obtainToken(@PathVariable(value = "username") String username, @PathVariable(value = "password") String password)
+    {
+        User user = userRepository.findByPseudoAndPassword(username,password);
+        //user.setToken(token);
+        userRepository.save(user);
+    }
+
+    @RequestMapping(value = "/deleteToken/{username}/{password}/{token}",method = RequestMethod.DELETE)
+    public void deleteToken(@PathVariable(value = "username")String username,@PathVariable(value = "password")String password,@PathVariable(value = "token")String token)
+    {
+        User user = userRepository.findByPseudoAndPasswordAndToken(username,password,token);
+        user.setToken(null);
+        userRepository.save(user);
+    }
+
+    @RequestMapping("/{username}")
+    public Iterable<User> getByPseudo(@PathVariable(value = "username") String pseudo)
+    {
+        return userRepository.findByPseudo(pseudo);
+    }
+
+    @RequestMapping(value = "/{username}/{newUsername}/{token}",method = RequestMethod.PUT)
+    public void setPseudo(@PathVariable(value = "username") String pseudo,@PathVariable(value = "newUsername")String nouveauPseudo,@PathVariable(value = "token") String token)
+    {
+        User user =userRepository.findByPseudoAndToken(pseudo,token);
+        user.setPseudo(nouveauPseudo);
+        userRepository.save(user);
+    }
+
+    @RequestMapping("/{id}")
+    public User findById(@PathVariable(value = "id") long id)
+    {
+        return userRepository.findOne(id);
+    }
+
 
 }
