93c02cb8225ef932ea62d3780f57a3eef490c7d7
Jérôme Deuchnord <astrofacile@gmail.com>
2016-12-10 14:46:51 +0100

    Ajout de la gestion de la session

--- src/main/java/fr/tanghevandekadsye/jee/controller/html/UserHtmlController.java
+++ src/main/java/fr/tanghevandekadsye/jee/controller/html/UserHtmlController.java
@@ -5,9 +5,11 @@ import fr.tanghevandekadsye.jee.Interfaces.Repository.UserRepository;
 import fr.tanghevandekadsye.jee.SocialNetwork;
 import fr.tanghevandekadsye.jee.entity.TwitterSocialNetwork;
 import fr.tanghevandekadsye.jee.entity.User;
+import org.apache.catalina.servlet4preview.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -39,6 +41,26 @@ public class UserHtmlController {
         return "subscribe_success";
     }
 
+    @RequestMapping("/login")
+    public String login(ModelMap model) {
+        model.addAttribute("error-credentials", false);
+
+        return "login-form";
+    }
+
+    @RequestMapping(value = "/login", method = RequestMethod.POST)
+    public String login(HttpServletRequest request, ModelMap model, @RequestParam String pseudo, @RequestParam String password) {
+        User user = userRepository.findByPseudoAndPassword(pseudo, password);
+        if(user != null) {
+            request.getSession().setAttribute("user", user);
+            model.addAttribute("error-credentials", true);
+
+            return "login-form";
+        }
+
+        return "redirect:/";
+    }
+
     @RequestMapping(value = "/edit-profile")
     public String editUser() {
         return "edit_profile";
@@ -52,8 +74,8 @@ public class UserHtmlController {
     }
 
     @RequestMapping(value = "/add-twitter", method = RequestMethod.POST)
-    public String addTwitterSocialNetwork(@ModelAttribute TwitterSocialNetwork twitterSocialNetwork) {
-        User user; // TODO récupérer l'utilisateur courant
+    public String addTwitterSocialNetwork(HttpServletRequest request, @ModelAttribute TwitterSocialNetwork twitterSocialNetwork) {
+        User user = (User) request.getSession().getAttribute("user"); // TODO récupérer l'utilisateur courant
 
         user.addSocialNetwork(twitterSocialNetwork);
         userRepository.save(user);
